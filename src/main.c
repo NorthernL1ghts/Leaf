4059f250e81e9979c2cb0385bb4c3f75609c33bd
NorthernL1ghts <noreply@local>
2026-02-21 10:22:36 +0000

    Read file path from argv and print contents to console

--- src/main.c
+++ src/main.c
@@ -1,5 +1,68 @@
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-    return 0;
+long file_size(FILE* file) {
+    if (!file) { return 0; }
+    fpos_t original = 0;
+    if (fgetpos(file, &original) != 0) {
+        printf("fgetpos() failed: %i\n", errno);
+        return 0;
+    }
+    fseek(file, 0, SEEK_END);
+    long out = ftell(file);
+    if (fsetpos(file, &original) != 0) {
+        printf("fsetpos() failed: %i\n", errno);
+    }
+    return out;
+}
+
+char* file_contents(char* path) {
+    FILE* file = fopen(path, "r");
+    if (!file) {
+        printf("Error: Could not open file %s\n", path);
+        return NULL;
+    }
+    long size = file_size(file);
+    char* contents = malloc(size + 1);
+    assert(contents && "Could not allocate buffer for file contents");
+    char* write_it = contents;
+    size_t bytes_read = 0;
+    while (bytes_read < (size_t)size) {
+        size_t bytes_read_this_iteration = fread(write_it, 1, (size_t)size - bytes_read, file);
+        if (ferror(file)) {
+            printf("Error: Could not read file %s\n", path);
+            free(contents);
+            fclose(file);
+            return NULL;
+        }
+
+        bytes_read += bytes_read_this_iteration;
+        write_it += bytes_read_this_iteration;
+
+        if (feof(file)) { break; }
+    }
+    contents[bytes_read] = '\0';
+    fclose(file);
+    return contents;
+}
+
+void print_usage(char** argv) {
+    printf("USAGE: %s <path_to_file_to_compile>\n", argv[0]);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        print_usage(argv);
+        exit(0);
+    }
+
+    char* path = argv[1];
+    char* contents = file_contents(path);
+    if (contents) {
+        printf("Contents of %s:\n--------------------------------\n%s\n--------------------------------\n", path, contents);
+        free(contents);
+    }
+    return contents ? 0 : 1;
 }

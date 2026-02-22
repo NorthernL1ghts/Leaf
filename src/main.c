9f97212efccde3b5894cc11c29f3eff00f97135f
NorthernL1ghts <noreply@local>
2026-02-22 11:56:52 +0000

    Introduce Token structure with token_create, print_tokens, and lex(Token*)

--- src/main.c
+++ src/main.c
@@ -111,24 +111,54 @@ void print_error(Error err) {
 const char *whitespace = " \r\n";
 const char *delimiters = " \r\n,{}():";
 
+typedef struct Token {
+    char* beginning;
+    char* end;
+    struct Token* next;
+} Token;
+
+Token* token_create() {
+    Token* token = malloc(sizeof(Token));
+    assert(token && "Could not allocate memory for token");
+    memset(token, 0, sizeof(Token));
+    return token;
+}
+
+void print_tokens(Token* root) {
+    size_t count = 1;
+    while (root) {
+        if (count > 10000) { break; } // FIXME: Remove this limit.
+        printf("Token %zu: ", count);
+        if (root->beginning && root->end) {
+            printf("%.*s", root->end - root->beginning, root->beginning);
+        }
+        putchar('\n');
+        root = root->next;
+        count++;
+    }
+}
+
 /// Lex the next token from SOURCE, and point to it with BEG and END.
-Error lex(char* source, char** beg, char** end) {
+Error lex(char* source, Token* token) {
     Error err = ok;
-    if (!source || !beg || !end) {
+    if (!source || !token) {
         ERROR_PREP(err, ERROR_ARGUMENTS, "Can not lex empty source.");
         return err;
     }
-    *beg = source;
-    *beg += strspn(*beg, whitespace); // Skip whitespace
-    if (**end == '\0') { return err; }
-    *end = *beg;
-    *end += strcspn(*beg, delimiters); // Skip to next delimiter
-    if (*end == *beg) {
-        *end += 1;
+    token->beginning = source;
+    token->beginning += strspn(token->beginning, whitespace); // Skip whitespace
+    token->end = token->beginning;
+    if (*(token->end) == '\0') { return err; }
+    token->end += strcspn(token->end, delimiters); // Skip to next delimiter
+    if (token->end == token->beginning) {
+        token->end += 1;
     }
     return err;
 }
 
+// TODO:
+// |-- API to create a new Node.
+// `-- API to add node as child.
 typedef long long integer_t;
 typedef struct Node {
     enum NodeType {
@@ -136,12 +166,10 @@ typedef struct Node {
         NODE_TYPE_INTEGER,
         NODE_TYPE_MAX,
     } type;
-    
     union NodeValue {
         integer_t integer;
     } value;
-
-    struct Node* children[3];
+    struct Node** children;
 } Node;
 
 #define nonep(node)    ((node).type == NODE_TYPE_NONE)
@@ -168,13 +196,25 @@ void environment_set() {
 }
 
 Error parse_expr(char* source, Node* result) {
-    char* beg = source;
-    char* end = source;
+    Token* tokens = NULL;
+    Token current_token;
+    current_token.next = NULL;
+    current_token.beginning = source;
+    current_token.end = source;
     Error err = ok;
-    while ((err = lex(end, &beg, &end)).type == ERROR_NONE) {
-        if (end - beg == 0) { break;}
-        printf("lexed: %.*s\n", end - beg, beg);
+    while ((err = lex(current_token.end, &current_token)).type == ERROR_NONE) {
+        if (current_token.end - current_token.beginning == 0) { break;}
+        
+        Token* rest_of_tokens = tokens;
+        tokens = token_create();
+        memcpy(tokens, &current_token, sizeof(Token));
+        tokens->next = rest_of_tokens;
+
+        printf("lexed: %.*s\n", current_token.end - current_token.beginning, current_token.beginning);
     }
+
+    print_tokens(tokens);
+
     return err;
 }
 
